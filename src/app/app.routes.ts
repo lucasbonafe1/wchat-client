9170336bcf61c65853fa97fbc7c3e30a889d6209
Lucas Bonafé <lucas.bonafe.prestserv@transpetro.com.br>
2025-12-29 20:32:19 -0300

    feat: melhoria na home page e rota adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from '../pages/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'chat', component: ChatComponent },
+];
